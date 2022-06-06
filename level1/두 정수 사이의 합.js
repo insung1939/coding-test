1dc5c11d5f90867b924c6ca7063fd8005e21480b
insung1939 <insungcho1939@gmail.com>
2022-06-07 06:48:51 +0900

    두 정수 사이의 합

---
+++ level1/두 정수 사이의 합.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+  let answer = 0;
+  const max = Math.max(a, b);
+  const min = Math.min(a, b);
+  for (let i = min; i <= max; i++) {
+    answer += i;
+  }
+  return answer;
+}
